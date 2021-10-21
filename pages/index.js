06feece72b20da991025bfa4d6434dfec0625ea0
Nujood <shawar.nujood@gmail.com>
2021-10-21 14:50:56 +0300

    Update the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const LandingPage = () => (
-  <div>Hello Mommy</div>
-) 
+  <Container>
+    <Box borderRadius="lg" bg="gray" p={3} mb={3} align="center">
+      Hey, I&apos;m a backend engineer based in Palestine
+    </Box>
+    <Box display={{ md: 'flex' }}>
+      <Box flexGrow={1}>
+        <Heading as="h2" variant="page-title">
+          Nujood Kadhem
+        </Heading>
+        <p>Learner (Mechatronics Enginner, Robotics, Developer)</p>
+      </Box>
+    </Box>
+  </Container>
+)
 
 export default LandingPage
