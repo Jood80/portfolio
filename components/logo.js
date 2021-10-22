a582b70b9e4137701472352108510e428fa06525
Nujood <shawar.nujood@gmail.com>
2021-10-22 19:39:12 +0300

    Style: Linting

--- components/logo.js
+++ components/logo.js
@@ -6,14 +6,14 @@ import styled from '@emotion/styled'
 const LogoBox = styled.span`
   font-weight: bold;
   font-size: 18px;
-  display: inline-flex
+  display: inline-flex;
   align-items: center;
   height: 30px;
   line-height: 20px;
   padding: 10px;
-   
+
   &:hover img {
-    transform: rotate(20deg)
+    transform: rotate(20deg);
   }
 `
 
@@ -26,7 +26,7 @@ const Logo = () => {
           <Image src={footPrintImg} width={20} height={20} alt="LOGO" />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-            fontFamily="M PLUS rounded 1c"
+            fontFamily="M PLUS Rounded 1c"
             fontWeight="bold"
             ml={3}
           >
