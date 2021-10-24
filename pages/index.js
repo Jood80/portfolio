49a9e34699268037ea7201c7b21162539b2bc1fa
Nujood <shawar.nujood@gmail.com>
2021-10-24 20:09:27 +0300

    Feat: add a paragraph to the hero section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  Link,
+  Button,
+  useColorModeValue
+} from '@chakra-ui/react'
+import NextLink from 'next/link'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 
 const LandingPage = () => (
   <Container>
-    <Box borderRadius="lg" bg="gray" p={3} mb={6} align="center">
+    <Box
+      borderRadius="lg"
+      bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+      p={3}
+      mb={6}
+      align="center"
+    >
       Hey, I&apos;m a backend engineer based in Palestine
     </Box>
     <Box display={{ md: 'flex' }}>
@@ -12,7 +30,48 @@ const LandingPage = () => (
         </Heading>
         <p>Learner (Mechatronics Enginner, Robotics, Developer)</p>
       </Box>
+      <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+        <Image
+          borderColor="whiteAlpha.800"
+          borderWidth={2}
+          borderStyle="solid"
+          maxWidth="100px"
+          display="inline-block"
+          borderRadius="full"
+          src="/images/jood.jpeg"
+          width={20}
+          height={20}
+          alt="my avatar"
+        />
+      </Box>
     </Box>
+    <Section delay={0.1}>
+      <Heading as="h3" variant="section-title">
+        Work
+      </Heading>
+      <Paragraph>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum{' '}
+        <NextLink href="/works/blogger">
+          <Link>bloger</Link>
+        </NextLink>
+        .
+      </Paragraph>
+      <Box align="center" my={4}>
+        <NextLink href="/works">
+          <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+            My portfolio
+          </Button>
+        </NextLink>
+      </Box>
+    </Section>
   </Container>
 )
 
