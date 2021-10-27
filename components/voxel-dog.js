2fa0dd684f1e68b7c71dda87e3a8297d25613415
Nujood <shawar.nujood@gmail.com>
2021-10-27 21:38:16 +0300

    Feat(dog): integrate the dog model into the Layout

---
+++ components/voxel-dog.js
@@ -0,0 +1,88 @@
+import { useState, useRef, useEffect } from 'react'
+import { Box } from '@chakra-ui/react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { loadGLTFModel } from '../lib/model'
+
+const VoxelDog = () => {
+  const refContainer = useRef()
+  const [renderer, setRenderer] = useState()
+  const [_camera, setCamera] = useState()
+  const [target] = useState(new THREE.Vector3(-0.5, 1.2, 0))
+  const [initialCameraPosition] = useState(
+    new THREE.Vector3(
+      20 * Math.sin(0.2 * Math.PI),
+      10,
+      20 * Math.cos(0.2 * Math.PI)
+    )
+  )
+
+  const [scene] = useState(new THREE.Scene())
+  const [_controls, setControls] = useState()
+
+  useEffect(() => {
+    const { current: container } = refContainer
+    if (container && !renderer) {
+      const scW = container.clientWidth
+      const scH = container.clientHeight
+
+      const renderer = new THREE.WebGL1Renderer({
+        antialias: true,
+        alpha: true
+      })
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(scW, scH)
+      renderer.outputEncoding = THREE.sRGBEncoding
+      container.appendChild(renderer.domElement)
+      setRenderer(renderer)
+
+      // 640 > 240 && 8 > 6
+      const scale = scH * 0.005 + 4.8
+      const camera = new THREE.OrthographicCamera(
+        -scale,
+        scale,
+        scale,
+        -scale,
+        0.01,
+        50000
+      )
+      camera.position.copy(initialCameraPosition)
+      camera.lookAt(target)
+      setCamera(camera)
+
+      const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
+      scene.add(ambientLight)
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.autoRotate = true
+      controls.target = target
+      setControls(controls)
+      loadGLTFModel(scene, '/dog.glb', {
+        receiveShadow: false,
+        castShadow: false
+      }).then(() => {
+        renderer.render(scene, camera)
+      })
+
+      return () => {
+        renderer.dispose()
+      }
+    }
+  }, [])
+
+  return (
+    <Box
+      ref={refContainer}
+      className="voxel-dog"
+      m="auto"
+      mt={['-20px', '-60px', '-120px']}
+      mb={['-40px', '-14px', '-200px']}
+      w={[280, 480, 640]}
+      h={[280, 480, 640]}
+      position="relative"
+    >
+      Doggyyyyy
+    </Box>
+  )
+}
+
+export default VoxelDog
